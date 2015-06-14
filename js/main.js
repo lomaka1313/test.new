8391e743e30b8a7e95433139f3abfa00d0c63dc2
Lomachuk Igor <lomaka13@ukr.net>
2015-06-15 01:13:42 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -12,5 +12,18 @@ $(document).ready(function(){
     });
     $(".user-block span").on("click", function(){
         $(this).siblings(".dropdown").slideToggle();
+    });
+    $(".small-logo").on("click", function(){
+        $(this).toggleClass("open")
+        var nav = $("nav");
+        nav.slideToggle(1000);
+        setTimeout(function() {
+            if (nav.css("display") == "block" && nav.css("owerflow") != "hidden") {
+                nav.css("display", "inline-block")
+            }
+        }, 1000)
+    });
+    $(".right-block-mobile").on("click", function(){
+        $(".sub-block").slideToggle();
     })
 })
